5c18bb9afaf82b45e98f37fa8fa084ac90d3497e
Isaac Hann <isaachann94@gmail.com>
2023-04-29 13:32:55 -0500

    Added DynamoDB integration. Now the bot can query DynamoDB tales for NPC names, descriptions, and pictures.

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # Clockwork-Familiar
-A simple Discord bot running out of an EC2 instance that contributes to a Dungeons and Dragons session in various ways.
+A Discord bot that helps you run Dungeons & Dragons (or other tabletop rpg) sessions. It runs out of an AWS EC2 Instance, queries NPC and Description tables out of DynamoDB, and plays locally stored mp3 music and/or ambience over the voice channel.
+
+## Current Features
+The bot currently supports the following text commands:
+```
+    !help - Shows all available commands
+    !tracks <tag> - Show list of audio tracks matching <tag>
+    !play <id> - Play audio track matching <id>
+    !stop - Stop playing audio and leave the voice channel (this also happens by default if the voice channel is empty)
+    !show npctables - Shows NPC Tables that can be loaded from DynamoDB
+    !load npctable <i> - Loads an NPC Table with table name partially matching <i>
+    !npc <i> - Describes an NPC with name partially matching <i> from the currently loaded table
+```
 
 #Dependencies
 ```
@@ -7,28 +19,35 @@ A simple Discord bot running out of an EC2 instance that contributes to a Dungeo
   @discord.js: 13.6.0
   fs: 0.0.1
   path: 0.12.7
+  aws-sdk: 2.1368.0
 ```
 
-
-## Config
+## Config File
 clockwork_familiar_config.json:
 ```
   {
     "token": "<Your bot token>",
     "prefix": "!",
-    "bot_channel_id": "<Your text channel id>",
-    "voice_channel_id": "<Your voice channel id>"
+    "commands_channel_id": "<Your channel id for bot commands>",
+    "npcs_channel_id": "<Your channel id for npc descriptions and pictures>",
+    "descriptions_channel_id": "<Your channel id for location descriptions">,
+    "voice_channel_id": "<Your channel id for voice>",
+    "npc_tables": [
+	    "<Name of DynamoDB NPC table>",
+	    "<Name of DynamoDB NPC table>",
+        ...
+    ],
+    "aws_region": "<AWS region where DynamoDB tables are located>"
   }
-  ```
-  
-  
-## Add bot to Discord server
+```
+
+## Adding the bot to your Discord server
   1. Select the bot in the Discord Developer Portal and go to OAuth2 -> URL Generator. Check the boxes for "bot" and "applications.commands". Copy the URL and paste it into the browser. Select the Discord server for the bot to join.
-  2. In clockwork_familiar.config.json, update "bot_channel_id" to the ID of the text channel where you want the bot to read commands and send messages. Update "voice_channel_id" to the ID of the voice channel where you want the bot to play music.
-  
-  
-## AWS EC2 Setup
-  1. Launch an Ubuntu EC2 instance (free tier) with the User Data:
+  2. In clockwork_familiar.config.json, update the four channel_id values to the channels you want the bot to be using in your Discord server (right-click a channel and select "Copy Channel ID"). The "commands" channel is a text channel where you will deliver commands to the bot. The "npcs" channel is a text channel where the bot will enter NPC names, descriptions, and pictures. The "descriptions" channel is a text channel where the bot will enter location descriptions. The "voice" channel id is where the bot will place music.
+
+
+## AWS - EC2 Instance Setup
+  1. Launch an Ubuntu EC2 instance (free tier) with the following User Data:
   ```
         sudo apt update
         sudo apt install nodejs
@@ -40,12 +59,12 @@ clockwork_familiar_config.json:
         npm install pm2
   ```
   2. Set up an SSH connection to your EC2 instance (look up tutorials if needed).
-  3. Copy the following files to your npm directory (I use WinSCP for this, look up tutorials if needed:
+  3. Copy the following files to your /home/ubuntu/ directory (I use WinSCP for this, look up a tutorial if needed):
   ```
         clockwork_familiar.js
         clockwork_familiar_config.json
   ```
-  4. Create a directory in /npm called "audio tracks" and fill it with mp3 files of songs you want to use for your game (again, I use WinSCP). Optionally, make songs searchable via Tag by adding tags separated by commas in parentheses at the end of the song name.
+  4. Create a directory in /home/ubuntu called "audio tracks" and fill it with mp3 files of songs you want to use for your game (again, I use WinSCP). Optionally, make songs searchable via Tag by adding tags separated by commas in parentheses at the end of the song name.
      Example mp3 files:
   ```
         Barovian Village (Town, Night).mp3
@@ -53,27 +72,22 @@ clockwork_familiar_config.json:
         Winter Woods (Forest, Snow).mp3
   ```
 
+
+## AWS - DynamoDB Setup
+  1. You can create as many NPC tables as you wish. Make sure they are all in the same AWS region, and the region is correctly noted in clockwork_familiar_config.json. 
+  2. The items in the tables should have a Name (string partition key - name of the NPC), Description (string), and Picture (string - an imgur link).
+  3. Create a Role for your EC2 Instance to access DynamoDB with ReadOnly privileges (look up a tutorial if needed)
   
+
 ## Running the Bot in EC2
-  1. In the npm directory, use the CLI to run the command:
+  1. In the /home/ubuntu directory, use the CLI to run the command:
   ```
-        pm2 start clockwork_familiar.js --max-memory-restart 100M
+        pm2 start clockwork_familiar.js
   ```
-  2. (Optional) Add a max memory threshold:
+  2. (Optional) You can include a scheduled daily restart:
   ```
-        --max-memory-restart 100M
-  ```
-  3. (Optional) Add a scheduled daily restart:
-  ```
-        --cron-restart="0 8 * * 0-7"
+        pm2 start clockwork_familiar.js --cron-restart="0 8 * * 0-7"
   ```
   
   
-## Using the Bot in Discord
-The bot currently supports the following text commands:
-```
-    !help - Shows all available commands
-    !tracks <tag> - Show list of audio tracks matching <tag>
-    !play <id> - Play audio track matching <id>
-    !stop - Stop playing audio and leave the voice channel (this also happens by default if the voice channel is empty)
-```
+
--- clockwork_familiar.js
+++ clockwork_familiar.js
@@ -1,13 +1,16 @@
-
-
 //Dependencies
 const { Client, Intents } = require('discord.js');
 const { createAudioPlayer, createAudioResource, joinVoiceChannel, AudioPlayerStatus, getVoiceConnection } = require('@discordjs/voice');
+const { prefix, token, commands_channel_id, npcs_channel_id, descriptions_channel_id, voice_channel_id, npc_tables, aws_region } = require('./clockwork_familiar_config.json');
 const fs = require('fs');
 const path = require('path');
 
-//Config File
-const { prefix, token, text_channel_id, voice_channel_id } = require('./clockwork_familiar_config.json');
+//AWS
+process.env['AWS_REGION'] = aws_region;
+const AWS = require('aws-sdk');
+
+//Document Client for DynamoDB
+const docClient = new AWS.DynamoDB.DocumentClient();
 
 //Audio player
 const audioPlayer = createAudioPlayer();
@@ -16,9 +19,8 @@ const audioPlayer = createAudioPlayer();
 var current_song = {
     track_index: 'null'
 }
-//Track if audio is can be looped
+//Track if audio can be looped
 var audioCanLoop = true;
-
 
 //Listener to loop audio when it finishes, if it hasn't been stopped
 audioPlayer.on(AudioPlayerStatus.Idle, () => {
@@ -26,8 +28,6 @@ audioPlayer.on(AudioPlayerStatus.Idle, () => {
         play(current_song.track_index);
     }
 })
-
-
 
 //Audio track table parameters
 const table = {
@@ -91,7 +91,12 @@ fs.readdir(audioFolder, function (err, files) {
             }
         }
     })
-})
+}) 
+
+//Array for holding npcs
+var npcs = [];
+
+
 
 
 //Log into Discord
@@ -101,7 +106,8 @@ client.once('ready', function () {
     console.log('Ready!');
 
     //Login message
-    client.channels.cache.get(text_channel_id).send("I am alive.");
+    client.channels.cache.get(commands_channel_id).send("I am alive.");
+
 
     //Check every 10 seconds to see if anyone is still in the voice channel
     setInterval(() => {
@@ -109,7 +115,7 @@ client.once('ready', function () {
         const connection = getVoiceConnection(voiceChannel.guild.id);
 
         if ((connection) && (voiceChannel.members.size <= 1)) {
-            client.channels.cache.get(text_channel_id).send("No listeners remain. Leaving the voice channel.");
+            client.channels.cache.get(commands_channel_id).send("No listeners remain. Leaving the voice channel.");
             audioCanLoop = false;
             audioPlayer.stop();
             const connection = getVoiceConnection(voiceChannel.guild.id);
@@ -136,7 +142,7 @@ client.on('messageCreate', async (message) => {
     }
 
     //Ignore commands outside of clockwork-familiar channel
-    if (message.channel.id !== text_channel_id) {
+    if (message.channel.id !== commands_channel_id) {
         return;
     }
 
@@ -147,25 +153,27 @@ client.on('messageCreate', async (message) => {
 
     //!help
     if (message.content.startsWith("!help")) {
-        return client.channels.cache.get(text_channel_id).send("```" + '\n' +
+        return client.channels.cache.get(commands_channel_id).send("```" + '\n' +
             "COMMANDS" + '\n' +
             '\n' +
             "Name            |Description                              |" + '\n' +
-            "----------------|-----------------------------------------|" + '\n' +
-            "!tracks         |Show full list of audio tracks           |" + '\n' +
-            "!tracks <tag>   |Show list of audio tracks matching <tag> |" + '\n' +
-            "!play <id>      |Play audio track matching <id>           |" + '\n' +
-            "!stop           |Stop playing audio                       |" + '\n' +
+            "----------------   |-----------------------------------------|" + '\n' +
+            "!tracks            |Show full list of audio tracks           |" + '\n' +
+            "!tracks <tag>      |Show list of audio tracks matching <tag> |" + '\n' +
+            "!play <id>         |Play audio track matching <id>           |" + '\n' +
+            "!stop              |Stop playing audio                       |" + '\n' +
+            "!show npctables    |Show NPC Tables that can be loaded       |" + '\n' +
+            "!load npctable <i> |Load NPC Table matching <i>              |" + '\n' +
+            "!npc <i>           |Describe NPC with name matching <i>      |" + '\n' +
             "```");
     }
-
 
     //!play <id>
     if (message.content.startsWith("!play")) {
 
         //Check that user is in a voice channel
         if (!message.member.voice.channel) {
-            return client.channels.cache.get(text_channel_id).send("You must be in a voice channel in order for me to play music!");
+            return client.channels.cache.get(commands_channel_id).send("You must be in a voice channel in order for me to play music!");
         }
 
         //Get the track ID
@@ -175,7 +183,7 @@ client.on('messageCreate', async (message) => {
 
         //Check that the track exists
         if (!playlist[track_index]) {
-            return client.channels.cache.get(text_channel_id).send("There is no audio track with ID " + track_input);
+            return client.channels.cache.get(commands_channel_id).send("There is no audio track with ID " + track_input);
         }
 
         //Save the selected track
@@ -215,7 +223,7 @@ client.on('messageCreate', async (message) => {
 
             //If nothing has the tag, return
             if (matching_songs.length < 1) {
-                return client.channels.cache.get(text_channel_id).send("No tracks have that tag.");
+                return client.channels.cache.get(commands_channel_id).send("No tracks have that tag.");
             }
 
             //Build a tracks table from those songs and display it
@@ -223,7 +231,6 @@ client.on('messageCreate', async (message) => {
         }
 
     }
-
 
     //!stop
     if (message.content.startsWith("!stop")) {
@@ -236,6 +243,25 @@ client.on('messageCreate', async (message) => {
         } catch (err) {
             console.log(err);
         }
+    }
+
+    //!show npctables
+    if (message.content.startsWith("!show npctables")) {
+            show_npc_tables();
+    }
+
+    //!load npctable <arg>
+    if (message.content.startsWith("!load npctable")) {
+        const argIndex = message.content.indexOf("npctable") + 8;
+        const arg = message.content.substring(argIndex).trim();
+        load_npc_table(arg);
+    }
+
+    //!npc <arg>
+    if (message.content.startsWith("!npc")) {
+        const argIndex = message.content.indexOf("!npc") + 4;
+        const arg = message.content.substring(argIndex).trim();
+        describe_npc(arg);
     }
 })
 
@@ -320,7 +346,7 @@ async function build_track_table(songs, x, y) {
     //If we need another message then we send this one and then build another
     if ((atMaxSize) && (index < songs.length)) {
         track_table = track_table + table.footer;
-        client.channels.cache.get(text_channel_id).send(track_table);
+        client.channels.cache.get(commands_channel_id).send(track_table);
 
         isFirstTable = false;
         build_track_table(songs, isFirstTable, index);
@@ -328,6 +354,108 @@ async function build_track_table(songs, x, y) {
     //Else if we don't need another message, send this one and stop
     else {
         track_table = track_table + table.footer;
-        client.channels.cache.get(text_channel_id).send(track_table);
+        client.channels.cache.get(commands_channel_id).send(track_table);
+    }
+}
+
+//Function for showing the available NPC tables
+async function show_npc_tables() {
+    try {
+        client.channels.cache.get(commands_channel_id).send("NPC Tables:");
+        for (var i = 0; i < npc_tables.length; i++) {
+            client.channels.cache.get(commands_channel_id).send((i + 1) + ". " + npc_tables[i]);
+        }
+    }
+    catch (err) {
+        client.channels.cache.get(commands_channel_id).send("Error in show_npc_tables(): " + err.message);
+    }
+}
+
+//Function for loading an NPC table
+async function load_npc_table(arg) {
+    try {
+        var possibleMatches = [];
+        var regex = new RegExp(arg, 'i');
+        var i = 0
+
+        //Find all possible matches for the input value
+        while (i < npc_tables.length) {
+            if (regex.test(npc_tables[i])) {
+                possibleMatches.push(npc_tables[i]);
+            }
+            i++;
+        }
+
+        //Input is not specific enough
+        if (possibleMatches.length > 1) {
+            client.channels.cache.get(commands_channel_id).send("I found " + possibleMatches.length + " NPC tables that match '" + arg + "'.");
+        }
+
+        //Input has no matches
+        else if (possibleMatches.length === 0) {
+            client.channels.cache.get(commands_channel_id).send("I did not find any NPC tables matching '" + arg + "'.");
+        }
+
+        //Input has exactly 1 match
+        else if (possibleMatches.length === 1) {
+            client.channels.cache.get(commands_channel_id).send("Loading NPC Table: " + possibleMatches[0]);
+            var tableNameString = possibleMatches[0];
+            var npcTableParams = {
+                TableName: tableNameString
+            };
+            docClient.scan(npcTableParams, function (err, data) {
+                if (err) {
+                    client.channels.cache.get(commands_channel_id).send("Error scanning NPC Table: " + err.message);
+                } else {
+                    npcs = data.Items;
+                    client.channels.cache.get(commands_channel_id).send('Loaded ' + npcs.length + ' NPCs from ' + tableNameString);
+                }
+            })
+        }
+    } catch (err) {
+        client.channels.cache.get(commands_channel_id).send("Error in load_npc_table(): " + err.message);
+    }
+}
+
+//Function for describing an NPC from the table
+async function describe_npc(arg) {
+    try {
+        var possibleMatches = [];
+        var regex = new RegExp(arg, 'i');
+        var i = 0
+        while (i < npcs.length) {
+            if (regex.test(npcs[i].Name)) {
+                possibleMatches.push(npcs[i]);
+            }
+            i++;
+        }
+        //Query is not specific enough
+        if (possibleMatches.length > 1) {
+            client.channels.cache.get(commands_channel_id).send("I found " + possibleMatches.length + " NPCs that match that name. Please be more specific.");
+        }
+        //Query has no matches
+        else if (possibleMatches.length === 0) {
+            client.channels.cache.get(commands_channel_id).send("I did not find any NPCs matching that name.");
+        }
+        //Query has exactly 1 match
+        else if (possibleMatches.length === 1) {
+            var selectedNPC = possibleMatches[0];
+            client.channels.cache.get(commands_channel_id).send("I found the NPC: " + selectedNPC.Name);
+            //Post picture
+            client.channels.cache.get(npcs_channel_id).send(
+              '\n' +
+              selectedNPC.Picture + '\n'
+            );
+            //Post name and description
+            client.channels.cache.get(npcs_channel_id).send(
+              '\n' + 
+              "**" + selectedNPC.Name + "**" + '\n' +
+              selectedNPC.Description + '\n' +
+              "----------------------------------------------------------"
+            );
+        }
+
+    } catch (err) {
+        client.channels.cache.get(commands_channel_id).send("Error in describe_npc(): " + err.message);
     }
 }
